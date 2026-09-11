3408ebf9f1b955056992731ff64bb77d0f0e32c2
Jeetu rajput <jeeturajput0302@gmail.com>
2026-09-11 16:28:03 +0530

    imagekit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ImageKitProvider } from '@imagekit/react'
 import './index.css'
 import App from './App.jsx'
 import { DataProvider } from './context/DataContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
+    <ImageKitProvider urlEndpoint="https://ik.imagekit.io/JeetuRajput">
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </ImageKitProvider>
   </StrictMode>,
 )
+
